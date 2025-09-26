39917d0dee525835573751d47b519a08a891b3f0
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-09-26 12:49:57 +0200

    Create Program.cs

---
+++ src/StudentManagement/Program.cs
@@ -0,0 +1,35 @@
+﻿using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StudentManagement.Infrastructure.Data;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        // Load variables from .env
+        Env.Load();
+
+        // Get connection string from env variable
+        var cs = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+        services.AddDbContext<StudentDbContext>(opt =>
+            opt.UseSqlServer(cs));
+
+        services.AddTransient<App>();
+    })
+    .Build();
+
+await host.Services.GetRequiredService<App>().RunAsync();
+
+public class App
+{
+    private readonly StudentDbContext _db;
+    public App(StudentDbContext db) => _db = db;
+
+    public async Task RunAsync()
+    {
+        await _db.Database.EnsureCreatedAsync();
+        Console.WriteLine("DB ready ✅");
+    }
+}
