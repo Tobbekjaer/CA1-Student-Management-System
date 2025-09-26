f4487a628d8c888b1237118fd1dfd4a7ba22ac2e
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-09-26 18:15:14 +0200

    feat(ef): V3 Add DateOfBirth (non-destructive) + script

---
+++ src/StudentManagement/ef-approach/artifacts/V3__AddDateOfBirth.sql
@@ -0,0 +1,8 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Student] ADD [DateOfBirth] datetime2 NOT NULL DEFAULT '0001-01-01T00:00:00.0000000';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250926160628_V3__AddDateOfBirth', N'9.0.9');
+
+COMMIT;
+GO
