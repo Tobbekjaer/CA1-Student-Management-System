e3b8d7d1bda66a416fe7867dc552f8963410872f
Tobbekjaer <tobias.hougs.kjaer@gmail.com>
2025-09-26 12:49:30 +0200

    Added Entities

---
+++ src/StudentManagement/Domain/Models/Course.cs
@@ -0,0 +1,8 @@
+namespace StudentManagement.Domain.Models;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Credits { get; set; }
+}
---
+++ src/StudentManagement/Domain/Models/Enrollment.cs
@@ -0,0 +1,9 @@
+namespace StudentManagement.Domain.Models;
+
+public class Enrollment
+{
+    public int Id { get; set; }
+    public int CourseId { get; set; }
+    public int StudentId { get; set; }
+    public string? Grade { get; set; }
+}
